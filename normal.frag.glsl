150434d0039a93e3b256d53635028fea96301e62
Charles Pigott <charlespigott@googlemail.com>
2015-04-14 15:39:00 +0100

    Add initial parameterised darkening/lightening of texture

--- normal.frag.glsl
+++ normal.frag.glsl
@@ -6,7 +6,10 @@ uniform sampler2D texture;
 // Interpolated texture coordinates recieved from vertex shader
 varying vec2 tex_position;
 
+const float STEP_SIZE = 18 / 255.0;
+
 void main(void) {
 	// Sets the fragment colour to the corresponding texel
-	gl_FragColor = texture2D(texture, tex_position);
+	vec4 col = texture2D(texture, tex_position);
+	gl_FragColor = max(col - vec4(4.0 * STEP_SIZE), vec4(0.0));
 }
